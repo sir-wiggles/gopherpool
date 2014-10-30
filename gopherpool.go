46a750d052304c0504a94581108b4fca9804a151
Jeff <jgregory@bitcasa.com>
2014-10-30 09:46:18 -0700

    comments

--- gopherpool.go
+++ gopherpool.go
@@ -26,9 +26,11 @@ type (
 		activeWorkers         int32
 		queueCapacity         int32
 		numberOfWorkers       int
+		processedJobs         uint64
 	}
 )
 
+// Starts all the threads needed for the pool
 func NewPool(numberOfWorkers int, queueCapacity int32) *Pool {
 	var workers int
 	if numberOfWorkers == 0 {
@@ -39,7 +41,7 @@ func NewPool(numberOfWorkers int, queueCapacity int32) *Pool {
 
 	pool := Pool{
 		queueChannel:          make(chan payload),
-		workChannel:           make(chan Work, workers),
+		workChannel:           make(chan Work, queueCapacity),
 		queueShutdownChannel:  make(chan bool),
 		workerShutdownChannel: make(chan bool, workers),
 		workerWaitGroup:       new(sync.WaitGroup),
@@ -49,7 +51,7 @@ func NewPool(numberOfWorkers int, queueCapacity int32) *Pool {
 
 	go pool.distributor()
 
-	fmt.Println(workers)
+	// +1 for the distributor
 	pool.workerWaitGroup.Add(workers + 1)
 	for workerID := 0; workerID < workers; workerID++ {
 		go pool.runWorker(workerID)
@@ -57,6 +59,7 @@ func NewPool(numberOfWorkers int, queueCapacity int32) *Pool {
 	return &pool
 }
 
+// Shutdown the pool
 func (pool *Pool) Shutdown() bool {
 	defer close(pool.queueChannel)
 	defer close(pool.workChannel)
@@ -66,32 +69,37 @@ func (pool *Pool) Shutdown() bool {
 	pool.queueShutdownChannel <- true
 
 	pool.workerWaitGroup.Wait()
-	fmt.Println("Pool closed")
+	fmt.Println("Gopherpool closed")
 
 	return true
 }
 
+// How you put message on the work queue
 func (pool *Pool) Push(w Work) bool {
-	queueItem := payload{w, make(chan bool, 1)}
+	queueItem := payload{w, make(chan bool)}
 	defer close(queueItem.result)
 	pool.queueChannel <- queueItem
 	return <-queueItem.result
 }
 
+// Returns the count of queued works
 func (pool *Pool) QueuedWork() int32 {
 	return atomic.AddInt32(&pool.queuedWork, 0)
 }
 
+// Returns the count of active workers
 func (pool *Pool) ActiveWorkers() int32 {
 	return atomic.AddInt32(&pool.activeWorkers, 0)
 }
 
+// Looping gopher pulling work off the work queue until something comes on the
+// shutdown channel
 func (pool *Pool) runWorker(id int) {
 	defer pool.workerWaitGroup.Done()
 	for {
 		select {
 		case <-pool.workerShutdownChannel:
-			fmt.Printf("Worker %d going down\n", id)
+			fmt.Printf("Gopher %3d sinking, %3d gophers remaining\n", id, pool.activeWorkers)
 			return
 		case work := <-pool.workChannel:
 			atomic.AddInt32(&pool.queuedWork, -1)
@@ -101,12 +109,15 @@ func (pool *Pool) runWorker(id int) {
 
 }
 
+// Where the actual work gets done
 func (pool *Pool) doWork(work Work) {
 	defer atomic.AddInt32(&pool.activeWorkers, -1)
 	atomic.AddInt32(&pool.activeWorkers, 1)
 	work.DoWork()
+	atomic.AddUint64(&pool.processedJobs, 1)
 }
 
+// Takes messages from the outside world and puts them on the work queue
 func (pool *Pool) distributor() {
 	defer pool.workerWaitGroup.Done()
 	for {
